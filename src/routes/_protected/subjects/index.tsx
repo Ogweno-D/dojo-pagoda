debc220a6b3c69eed5dd0a0142bdaaec42071fa1
Ogweno-D <dennisogwene@gmail.com>
2025-09-22 06:10:05 +0300

    Dojo-Pagoda:Routes

--- src/routes/_protected/subjects/index.tsx
+++ src/routes/_protected/subjects/index.tsx
@@ -1,14 +1,56 @@
 import { createFileRoute } from '@tanstack/react-router'
 import SubjectTable from "../../../components/Table/Subjects/SubjectsTable/SubjectTable.tsx";
+import type {Subject} from "../../../components/Table/Subjects/Subject.type.ts";
+import {DataTableProvider, useDataTable} from "../../../components/Table/providers/DataTableProvider.tsx";
+import type {Task} from "../../../components/Table/Tasks/Task.type.ts";
+import {buildQueryParams} from "../../../utils/queryParams.ts";
+import {useFetch} from "../../../hooks/api/useFetch.tsx";
+import {useEffect} from "react";
+import PaginationWrapper from "../../../components/Table/ReusableTable/TableActions/PaginationWrapper.tsx";
 
+export  interface SubjectApiResponse {
+    records: Subject[];
+    domain: string;
+    current_page: number;
+    total_count: number;
+    last_page: number;
+    page_size: number;
+}
 export const Route = createFileRoute('/_protected/subjects/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return(
-      <div>
-          <SubjectTable/>
-      </div>
-  )
+    return (
+        <DataTableProvider<Task>
+            data={[]}
+            initialState={{ filters: [], sorts: [], page: 1, pageSize: 5 }}
+        >
+            <SubjectTableWrapper />
+        </DataTableProvider>
+    );
+}
+function SubjectTableWrapper() {
+    const { page, pageSize, setData} = useDataTable<Subject>();
+
+    const params = { page, page_size: pageSize };
+    const url = `/api/admin/tasks${buildQueryParams(params)}`;
+
+    const { data, loading, error } = useFetch<SubjectApiResponse>(url, {
+        headers: { "Content-Type": "application/json" },
+    });
+
+    const subjects: Subject[] = data?.records ?? [];
+
+    // Update provider data when API returns
+    useEffect(() => {
+        setData(subjects);
+    }, [subjects, setData]);
+
+    return (
+        <>
+            <SubjectTable loading={loading} error={error} data={data} />
+            <PaginationWrapper data={data} />
+        </>
+    );
 }
--- src/routes/_protected/users/index.tsx
+++ src/routes/_protected/users/index.tsx
@@ -1,5 +1,20 @@
 import { createFileRoute } from '@tanstack/react-router'
 import UserTable from "../../../components/Table/Users/UsersTable/UserTable.tsx";
+import type {User} from "../../../components/Table/Users/User.type.ts";
+import {DataTableProvider, useDataTable} from "../../../components/Table/providers/DataTableProvider.tsx";
+import {buildQueryParams} from "../../../utils/queryParams.ts";
+import {useFetch} from "../../../hooks/api/useFetch.tsx";
+import {useEffect, useState} from "react";
+import PaginationWrapper from "../../../components/Table/ReusableTable/TableActions/PaginationWrapper.tsx";
+
+export interface UserApiResponse {
+  records: User[];
+  domain: string;
+  current_page: number;
+  total_count: number;
+  last_page: number;
+  page_size: number;
+}
 
 export const Route = createFileRoute('/_protected/users/')({
   component: RouteComponent,
@@ -7,6 +22,45 @@ export const Route = createFileRoute('/_protected/users/')({
 
 function RouteComponent() {
   return (
-      <UserTable />
-  )
+      <DataTableProvider<User>
+          data={[]}
+          initialState={{filters:[], sorts: [], page:1, pageSize:5}}
+        >
+
+      <UserTableWrapper />
+    </DataTableProvider>
+  );
+}
+
+function UserTableWrapper() {
+  const {page, pageSize, setData} = useDataTable<User>()
+  const [searchQuery, setSearchQuery] = useState('');
+  const [roleFilter, setRoleFilter] = useState('');
+  const [statusFilter, setStatusFilter] = useState('');
+
+  const params = {
+    page,
+    page_size: pageSize,
+    search: searchQuery,
+    role: roleFilter,
+    status: statusFilter,
+  }
+  const url = `/api/admin/users/${buildQueryParams(params)}`;
+
+  const { data, loading, error } = useFetch<UserApiResponse>(url,{
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const users: User[] = data?.records ?? [];
+
+  useEffect(() => {
+    setData(users)
+  }, [users, setData]);
+
+  return(
+      <>
+        <UserTable loading={loading} error={error} data={data}/>
+        <PaginationWrapper data={data} />
+        </>
+  );
 }
